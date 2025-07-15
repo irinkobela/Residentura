9b7789baa8d67509f06600e84ab67aece23fdd49
Irine Bakhutashvili <irinebakhutashvili@outlook.com>
2025-07-15 04:05:33 +0400

    "whatever"

--- src/components/ReviewDeck.js
+++ src/components/ReviewDeck.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useShortcuts } from '../contexts/ShortcutContext';
 import QuestionView from './QuestionView';
 import './ReviewDeck.module.css';
